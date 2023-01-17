c258f56f4c915cbbc1b301c1a44faab3fff909d9
LafereAngelique <122604814+LafereAngelique@users.noreply.github.com>
2023-01-17 13:19:20 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -5,8 +5,9 @@ basic.forever(function () {
         basic.pause(1000)
         tafel3 += 3
     } else {
-        basic.pause(1000)
         tafel3 = 0
+        basic.showNumber(tafel3)
+        basic.pause(1000)
     }
 })
 basic.forever(function () {
